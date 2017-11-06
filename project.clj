408112d774d46167e5b796ce122849c73cc13750
Rintaro Okamura <rintaro.okamura@gmail.com>
2017-11-06 22:19:46 +0900

    Remove useless aot

--- project.clj
+++ project.clj
@@ -3,6 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :aot [jetty-websocket-client.socket]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.eclipse.jetty.websocket/websocket-client "9.4.7.v20170914"]])
